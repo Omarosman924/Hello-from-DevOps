41a4162c9e1f339d8ff9c0e22983d8ba3c975a54
omarosman924 <omarosman924@gmail.com>
2025-05-08 01:27:38 +0200

    fix spaces

--- app.py
+++ app.py
@@ -5,11 +5,10 @@ app = Flask(__name__)
 
 # Define a route for the root URL
 @app.route('/')
-def hello_world():
-        return 'Hello, World!'
+def hello_world():  
+    return 'Hello, World!'
 
     # Run the application
-    if __name__ == '__main__':
-            app.run(debug=True)
-
+if __name__ == '__main__':
+     app.run(debug=True)
 
