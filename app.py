2a78fa8dbcce18bde28bf524722bf7baf84e7efd
omarosman924 <omarosman924@gmail.com>
2025-05-08 05:18:13 +0200

    added docker file and chagne flask app

--- app.py
+++ app.py
@@ -1,3 +1,4 @@
+
 from flask import Flask
 
 # Create a Flask application
@@ -5,10 +6,10 @@ app = Flask(__name__)
 
 # Define a route for the root URL
 @app.route('/')
-def hello_world():  
-    return 'Hello, World!'
+def hello_world():
+        return 'Hello, World!'
 
     # Run the application
 if __name__ == '__main__':
-     app.run(debug=True)
+    app.run(debug=True, host="0.0.0.0", port=5000)
 
